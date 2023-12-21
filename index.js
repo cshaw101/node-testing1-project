8afd9b03ef05c53682438453b3a9603c71d02ea6
caleb <shawcaleb1688@gmail.com>
2023-12-21 16:21:15 -0500

    5a and 5b are completed

--- index.js
+++ index.js
@@ -80,6 +80,8 @@ class Seasons {
    */
   constructor() {
     // ✨ initialize whatever properties are needed
+    this.seasons = ['summer', 'fall', 'winter', 'spring']
+    this.currentSeason = 0
   }
 
   /**
@@ -95,7 +97,13 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    const result = this.seasons[this.currentSeason]
+    if (this.currentSeason === 3) {
+      this.currentSeason = 0
+    } else {
+      ++this.currentSeason
+    }
+    return result
   }
 }
 
